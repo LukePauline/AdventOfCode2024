4d3bc77b6a056aa703c2918ab935d83a0b37b12e
lukep <lukepauline@gmail.com>
2024-12-03 18:53:27 +0000

    day 3

---
+++ AdventOFcODE2024.Tests/Day3Tests.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2024.Tests;
+public class Day3Tests : TestBase<Day3>
+{
+    public override string Exercise1TestInput => "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    public override object Exercise1TestResult => 161;
+
+    public override string Exercise2TestInput => "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+    public override object Exercise2TestResult => 48;
+}
---
+++ AdventOfCode2024/Day3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+public partial class Day3 : IDay
+{
+    public object Exercise1(string input)
+    {
+        var matches = ExtractData().Matches(input);
+        return matches.Sum(x => int.Parse(x.Groups[1].Value) * int.Parse(x.Groups[2].Value));
+    }
+
+    public object Exercise2(string input)
+    {
+        var reduced = GetDonts().Replace(input + "do()", "");
+        var matches = ExtractData().Matches(reduced);
+        return matches.Sum(x => int.Parse(x.Groups[1].Value) * int.Parse(x.Groups[2].Value));
+    }
+
+    [GeneratedRegex(@"mul\(([0-9]{1,3}),([0-9]{1,3})\)")]
+    private static partial Regex ExtractData();
+
+    [GeneratedRegex(@"(?<=don't\(\)).*?(?=do\(\))", RegexOptions.Singleline)]
+    private static partial Regex GetDonts();
+}
